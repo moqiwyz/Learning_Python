7cce9a74e0fc4bd04834804608f7072f76fdfc7a
moqiwyz <201482027+moqiwyz@users.noreply.github.com>
2025-07-04 11:57:39 +0100

    Add files via upload

---
+++ The Zip Function.py
@@ -0,0 +1,8 @@
+owners = ["Jenny", "Alexus", "Sam", "Grace"]
+dogs_names = ["Elphonse", "Dr. Doggy DDS", "Carter", "Ralph"]
+
+names_and_dogs_name = zip(owners, dogs_names)
+
+list_of_names_and_dogs_names = list(names_and_dogs_name)
+
+print(list_of_names_and_dogs_names)
